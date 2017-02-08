5b702336d2d14e62c936e82d0050a6960d0eae80
rpraval1 <rpraval1@binghamton.edu>
2017-02-08 15:17:39 -0500

    Boilerplate for meteor-semantic-react projects

---
+++ imports/ui/message.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Message } from 'semantic-ui-react'
+
+const ExampleMessage = () => (
+  <Message>
+    <Message.Header>
+      Test for semantic-ui
+    </Message.Header>
+    <p>
+      This is a test for meteor-react-semantic-ui boilerplate. You can now clone this and enjoy your project bulding with meteor-react-semantic-ui!!
+    </p>
+  </Message>
+)
+
+export default ExampleMessage
